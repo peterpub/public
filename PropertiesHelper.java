c3168fbccbaa928137ca88560bf50fed1d11d8d2
p <p@p.com>
2023-05-26 09:47:24 -0400

    dfdff

--- PropertiesHelper.java
+++ PropertiesHelper.java
@@ -1,37 +1,37 @@
-//package lib.public;
-//
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.util.Properties;
-//
-//public class PropertiesHelper {
-//
-//    public static String getValueFromPropertiesFile(String propertiesKey, String absolutePath) {
-//        Properties prop = new Properties();
-//        FileInputStream input = null;
-//        String value = null;
-//
-//        try {
-//            // Load the properties file from the given absolute path
-//            input = new FileInputStream(absolutePath);
-//            prop.load(input);
-//
-//            // Get the value of the specified key from the properties file
-//            value = prop.getProperty(propertiesKey);
-//
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        } finally {
-//            // Close the input stream
-//            if (input != null) {
-//                try {
-//                    input.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//
-//        return value;
-//    }
-//}
+package lib.git;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesHelper {
+
+    public static String getValueFromPropertiesFile(String propertiesKey, String absolutePath) {
+        Properties prop = new Properties();
+        FileInputStream input = null;
+        String value = null;
+
+        try {
+            // Load the properties file from the given absolute path
+            input = new FileInputStream(absolutePath);
+            prop.load(input);
+
+            // Get the value of the specified key from the properties file
+            value = prop.getProperty(propertiesKey);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            // Close the input stream
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return value;
+    }
+}
--- SqlHelper.java
+++ SqlHelper.java
@@ -1,4 +1,72 @@
-package lib.public;
+package lib.git;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 //
 public class SqlHelper {
+    static Connection _c = null;
+    public static void main(String[]z){
+
+    }
+    public static Connection c() {
+        try {
+            if (_c == null || _c.isClosed()) {
+                try {
+                    Class.forName("org.postgresql.Driver");
+                    _c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "admin",
+                            "admin");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                    System.exit(0);
+                }
+                // System.out.println("Opened database successfully");
+
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return _c;
+    }
+
+    public static CallableStatement callableStatement(String q){
+        try {
+            java.sql.CallableStatement s = c().prepareCall(q);
+            return s;
+        } catch (Exception e) {
+            for(StackTraceElement ste:e.getStackTrace()){
+                System.out.println(ste);
+            }
+        }
+        return null;
+    }
+
+    public static ResultSet executeQuery(String q) {
+        ResultSet rs = null;
+        try {
+            java.sql.CallableStatement s = c().prepareCall(q);
+            //  System.out.println(s.toString());
+            rs = s.executeQuery();
+            return rs;
+        } catch (Exception e) {
+            for(StackTraceElement ste:e.getStackTrace()){
+                System.out.println(ste);
+            }
+        }
+        return null;
+    }
+
+    public static boolean execute(String q) {
+        try {
+            c().createStatement().execute(q);
+            return true;
+        } catch (Exception ex) {
+
+        }
+        return false;
+    }
 }
